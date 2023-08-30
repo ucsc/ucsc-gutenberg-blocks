3287b2ad23912ebedd386016f9fa8d6e38cb681f
Kumar Agarwal <kagarwal@ucsc.edu>
2023-08-30 09:08:17 -0700

    only show unique emails on directory profile

--- templates/DirectoryProfileTemplate.php
+++ templates/DirectoryProfileTemplate.php
@@ -6,6 +6,8 @@ if ( file_exists( get_theme_file_path( 'header-plugin.php' ) ) ) {
 $cruzid = get_query_var('directoryprofilecruzid');
 $campusDirectoryAPI = new CampusDirectoryAPI($attributes);
 $profileData = $campusDirectoryAPI->getCampusDirData($cruzid,true)[0];
+$cruzidEmail = $profileData['0']['mail'][0];
+
 
 function linkify($key, $str) {
     if ($key == "ucscpersonpubwebsite") {
@@ -92,11 +94,15 @@ if (count($profileData)) {
 
 
                     <?php
+                        $seenEmails = [$cruzidEmail];
                         for($i=0; $profileData["ucscpersonpubalternatemail"] && $i<count($profileData["ucscpersonpubalternatemail"]) - 1; $i++) {
-                            echo "<p class='altemail'>";
-                            echo '<svg class="svg-inline--fa fa-envelope fa-w-16" title="User" aria-labelledby="svg-inline--fa-title-5" data-prefix="far" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><title id="svg-inline--fa-title-5">User</title><path fill="currentColor" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>';
-                                echo "<a href=\"mailto:{$profileData['ucscpersonpubalternatemail'][$i]}\">{$profileData['ucscpersonpubalternatemail'][$i]}</a>";
-                            echo "</p>";
+                            if (!in_array($profileData['ucscpersonpubalternatemail'][$i], $seenEmails)) {
+                                array_push($seenEmails, $profileData['ucscpersonpubalternatemail'][$i]);
+                                echo "<p class='altemail'>";
+                                echo '<svg class="svg-inline--fa fa-envelope fa-w-16" title="User" aria-labelledby="svg-inline--fa-title-5" data-prefix="far" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><title id="svg-inline--fa-title-5">User</title><path fill="currentColor" d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path></svg>';
+                                    echo "<a href=\"mailto:{$profileData['ucscpersonpubalternatemail'][$i]}\">{$profileData['ucscpersonpubalternatemail'][$i]}</a>";
+                                echo "</p>";
+                            }
                         }
                     ?>
 
