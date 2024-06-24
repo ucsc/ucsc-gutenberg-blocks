375643cfc028324cce03ec5421ea7f789b36739d
Jason Chafin <jchafin@ucsc.edu>
2024-06-24 10:53:57 -0700

    chore: version bump

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: UCSC Gutenberg Blocks
  * Plugin URI: https://github.com/ucsc/ucsc-gutenberg-blocks
  * Description: Custom UCSC Gutenberg Blocks.
- * Version: 1.1.25
+ * Version: 1.1.26
  * Author: UC Santa Cruz
  * Author URI: https://github.com/ucsc
  */
